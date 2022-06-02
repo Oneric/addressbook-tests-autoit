117a505cb30b6dc895586ee57bcd29fbb1697f57
Oneric <anton4er58@gmail.com>
2022-06-03 00:21:18 +0700

    Compare and Equals fix

--- addressbook-tests-autoit/Model/GroupData.cs
+++ addressbook-tests-autoit/Model/GroupData.cs
@@ -16,11 +16,23 @@ namespace addressbook_tests_autoit
 
         public int CompareTo(GroupData other)
         {
+            if (object.ReferenceEquals(other, null))
+            {
+                return 1;
+            }
             return this.Name.CompareTo(other.Name);
         }
 
         public bool Equals(GroupData other)
         {
+            if (object.ReferenceEquals(other, null))
+            {
+                return false;
+            }
+            if (object.ReferenceEquals(this, other))
+            {
+                return true;
+            }
             return this.Name.Equals(other.Name);
         }
     }
